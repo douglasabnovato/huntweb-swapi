ae7b07f02f953945fe933cd874b0031bccf7284d
Douglas A B Novato <douglasabnovato@gmail.com>
2022-08-03 14:52:39 -0300

    terefas

--- src/pages/persona/index.js
+++ src/pages/persona/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import api from "../../services/api";
 import "./styles.css";
 
 export default function Persona() {
